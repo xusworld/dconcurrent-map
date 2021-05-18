44c8c923fdd7b7462476ca40fee48bed09402b5f
lukedong <lukedong@tencent.com>
2021-05-18 15:55:01 +0800

    v0.1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xusworld/ConcurrentMap
+
+go 1.15
+
+require github.com/xusworld/infinity-go v0.0.0-20201228091545-814999706237
---
+++ hashmap/util.go
@@ -0,0 +1,12 @@
+package hashmap
+
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	const prime32 = uint32(16777619)
+	keyLength := len(key)
+	for i := 0; i < keyLength; i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
